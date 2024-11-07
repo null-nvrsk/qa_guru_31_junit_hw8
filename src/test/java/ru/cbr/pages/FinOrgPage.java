9932515fec47c183e1fb44111693b686ab2cb770
Maksim Skomorokhov <nullnvrsk@gmail.com>
2024-11-07 23:22:10 +0300

    fix unnecessary code from CBR-20

--- src/test/java/ru/cbr/pages/FinOrgPage.java
+++ src/test/java/ru/cbr/pages/FinOrgPage.java
@@ -34,6 +34,4 @@ public class FinOrgPage {
 
         return this;
     }
-
-
 }
--- src/test/java/ru/cbr/tests/FinOrgTests.java
+++ src/test/java/ru/cbr/tests/FinOrgTests.java
@@ -5,14 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import ru.cbr.pages.FinOrgPage;
 
 import static com.codeborne.selenide.Selenide.open;
 
 @DisplayName("Поиск организаций, участников финансового рынка")
 public class FinOrgTests extends BaseTest {
-
-    FinOrgPage finOrgPage = new FinOrgPage();
 
     @BeforeEach
     void setUp() {
@@ -22,8 +19,8 @@ public class FinOrgTests extends BaseTest {
     @ValueSource(strings = {
             "Все", "действующий", "недействующий"
     })
-    @ParameterizedTest(name = "Поиск организаций типа \"Бюро кредитных историй\" со статусом вида деятельности \"{0}\"" +
-            " должен показать не пустой список")
+    @DisplayName("Поиск организаций типа \"Бюро кредитных историй\" со статусом вида деятельности")
+    @ParameterizedTest(name = "Со статусом вида деятельности \"{0}\" должен показать не пустой список")
     @Tag("CBR-20")
     void searchFinOrgWithFilterByStatus(String status) {
         finOrgPage
@@ -33,4 +30,6 @@ public class FinOrgTests extends BaseTest {
 
         finOrgPage.verifyResultNotEmpty();
     }
+
+//    @CsvSource - CBR-19 Проверка по частичному названию организации ("ВТБ", "Альфа", "Петрокоммерц")
 }
