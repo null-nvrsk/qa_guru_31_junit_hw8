c8b8daa8c10372f6a728c5b22085407117cafb9a
Maksim Skomorokhov <nullnvrsk@gmail.com>
2024-11-09 12:55:08 +0300

    Remove unnecessary code

--- src/test/java/ru/cbr/models/CompanyInfo.java
+++ src/test/java/ru/cbr/models/CompanyInfo.java
@@ -4,15 +4,16 @@ import lombok.Data;
 
 @Data
 public class CompanyInfo {
+
+    String companyName;
+    String INN;
+    String OGRN;
+    String status;
+
     public CompanyInfo(String companyName, String INN, String OGRN, String status) {
         this.companyName = companyName;
         this.INN = INN;
         this.OGRN = OGRN;
         this.status = status;
     }
-
-    String companyName;
-    String INN;
-    String OGRN;
-    String status;
 }
--- src/test/java/ru/cbr/tests/BaseTest.java
+++ src/test/java/ru/cbr/tests/BaseTest.java
@@ -1,34 +1,19 @@
 package ru.cbr.tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import ru.cbr.pages.FinOrgPage;
 import ru.cbr.pages.components.OffsetMenu;
-
-import static com.codeborne.selenide.Selenide.$;
 
 public class BaseTest {
 
     FinOrgPage finOrgPage = new FinOrgPage();
     OffsetMenu offsetMenu = new OffsetMenu();
 
-    SelenideElement popupCookies = $(".popup-cookies");
-    SelenideElement popupCookiesConfirmButton = $(".popup-cookies .btn");
-
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://www.cbr.ru";
         Configuration.browserPosition = "0x0";
         Configuration.browserSize = "1920x1080";
-    }
-
-    public void clearCookies() {
-        Selenide.clearBrowserCookies();
-        Selenide.clearBrowserLocalStorage();
-
-        if (popupCookies.exists() && popupCookies.isDisplayed())
-            popupCookiesConfirmButton.click();
     }
 }
